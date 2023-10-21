f8b8c02b0ca9172a77eb4b1e21137edc9f5e66ae
Qnmusic <hahoangquan2@gmail.com>
2023-10-21 09:06:10 +0700

    version 1.0

---
+++ src/ts/script.ts
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (): void => {
+    console.log('hello my friends!');
+});
+
+
+const handleOnclickNavigationList = (target: string): void => {
+    if (target === "4") {
+        alert('Kmin xin gửi lời chào đến bạn, chúc bạn có 1 ngày tốt lành và gặp được nhiều may mắn.')
+    }
+};
+
+
